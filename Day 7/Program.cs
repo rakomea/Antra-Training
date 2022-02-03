bab7f6145348f7a072f71680596fe70b6cbafa54
Richard Akomea <55210364+rakomea@users.noreply.github.com>
2022-02-03 01:16:31 -0500

    Update Program.cs
    couple tweaks

--- Day 7/Program.cs
+++ Day 7/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 namespace CosoleApp2
 {
-  Date dt1 = new Date(1, 2, 2000);
+  ProgramMethod programMethod = new ProgramMethod();
+  public static void Main ()
+    {
+        int n = 9;
+        Console.WriteLine(fib(n));
+    Date dt1 = new Date(1, 2, 2000);
         Date dt2 = new Date(1, 2, 2004);
        
         // Function call
         Console.WriteLine("Difference between two dates is "
                           + getDifference(dt1, dt2));
+    }
 }
